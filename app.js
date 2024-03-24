178bcadda6fc9f9d2fe70487a995769c9a28086b
SouviK-xD <121093416+SouviK-xD@users.noreply.github.com>
2024-03-24 20:54:10 +0530

    Updated app

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function showError(error) {
 
 // GET WEATHER FROM API
 async function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     try {
         const response = await fetch(api);
         const data = await response.json();
