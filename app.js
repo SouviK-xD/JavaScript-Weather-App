79ffc6885c49dad6f3419c690cb915e29ebc5ea6
SouviK-xD <121093416+SouviK-xD@users.noreply.github.com>
2024-03-24 20:44:39 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+// Tutorial by http://youtube.com/CodeExplained
+// api key : 82005d27a116c2880c8f0fcb866998a0
+
+// SELECT ELEMENTS
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// APP DATA
+const weather = {};
+
+weather.temperature = {
+    unit: "celcius"
+};
+
+// APP CONST AND VARS
+const KELVIN = 273;
+// API KEY
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't support Geolocation</p>";
+}
+
+// SET USER'S POSITION
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error) {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p>${error.message}</p>`;
+}
+
+// GET WEATHER FROM API
+async function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    try {
+        const response = await fetch(api);
+        const data = await response.json();
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+        displayWeather();
+    } catch (error) {
+        console.error('Error fetching weather:', error);
+    }
+}
+
+// DISPLAY WEATHER TO UI
+function displayWeather() {
+    iconElement.innerHTML = `<img src ="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
